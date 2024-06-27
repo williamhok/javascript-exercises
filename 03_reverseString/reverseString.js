e682b3e6a397047385eaf6eacf5f5a0347dd4a32
william <173856982+williamhok@users.noreply.github.com>
2024-06-27 19:01:00 +1000

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let result = ""
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str.at(i)
+    }
+
+    return result
 };
 
 // Do not edit below this line
