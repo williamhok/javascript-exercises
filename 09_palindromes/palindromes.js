2ccd00e09efb3b5f8724701b1f77b9ef7ecbabd2
william <173856982+williamhok@users.noreply.github.com>
2024-07-23 19:00:41 +1000

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    const cleanedStr = str
+        .toLowerCase()
+        .split('')
+        .filter(char => alphanumerical.includes(char))
+        .join('')
+
+    const reverse = cleanedStr
+        .split('')
+        .reverse()
+        .join('')
+
+    return cleanedStr === reverse
 };
 
 // Do not edit below this line
