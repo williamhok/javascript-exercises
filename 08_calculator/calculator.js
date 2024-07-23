5d0dd4d9215e0ae8b17330a652a12948bacf968a
william <173856982+williamhok@users.noreply.github.com>
2024-07-23 18:52:40 +1000

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let total = 0
+	array.map(element => {
+    total += element
+  });
+  
+  return total
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 1
+  array.map(num => {
+    total *= num
+  })
 
+  return total
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let total = 1
+
+  for (let i = 0; i < b; i++) {
+    total *= a
+  }
+
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x == 0) {
+    return 1
+  }
+
+  let total = 1;
+
+  for (let i = x; i > 0; i--) {
+    total *= i
+  }
+
+  return total
 };
 
 // Do not edit below this line
